138a930811411ff85ef51693b6fee6db04787c20
Relmnight <Relmnight@Protonmail.com>
2020-10-25 20:13:18 +0100

    dunno

--- Receiver/CustomUntrigger.lua
+++ Receiver/CustomUntrigger.lua
@@ -1,14 +1,15 @@
-function(event, encounterID, name, difficulty, size, success)
-    if event == "ENCOUNTER_END" or (not aura_env.bossesOnly and event == "PLAYER_REGEN_ENABLED") then
-        print("sender untrigger")
-        local combatTime = aura_env.startTime and GetTimeStringFromSeconds(GetTime() - aura_env.startTime, false, true)
-        or 0
-        aura_env.startTime = nil
-        aura_env.encounterName = nil
-        aura_env.inEnc = nil
-        return true
-    end
-end
+-- Seemingly Unneeded
+-- function(event, encounterID, name, difficulty, size, success)
+--     if event == "ENCOUNTER_END" or (not aura_env.bossesOnly and event == "PLAYER_REGEN_ENABLED") then
+--         print("sender untrigger")
+--         local combatTime = aura_env.startTime and GetTimeStringFromSeconds(GetTime() - aura_env.startTime, false, true)
+--         or 0
+--         aura_env.startTime = nil
+--         aura_env.encounterName = nil
+--         aura_env.inEnc = nil
+--         return true
+--     end
+-- end
 
 
 
